56acb7b7ffcd16c26d7501dd4c0991187dee0745
sambhavchapagain <142641213+sambhavchapagain@users.noreply.github.com>
2023-08-19 02:49:28 -0400

    3add

---
+++ 3add.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+ {
+    float n1,n2,sum;
+ printf("enter two integers:");
+ scanf("%f %f",&n1,&n2);
+  sum=n1+n2;
+  printf("%f +%f=%f",n1,n2,sum);
+  return 0;
+  }
+
