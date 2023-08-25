1a9a618744f5a131fb811aaf537fe766e04732ae
sambhavchapagain <142641213+sambhavchapagain@users.noreply.github.com>
2023-08-25 14:10:37 -0400

    8_menu

--- 7 1 to 100 .c
+++ 7 1 to 100 .c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    for (int i = 1; i <= 100; i++) {
-        printf("%d\n", i);
+    for
+    (int i = 1; i <= 100; i++)
+     {
+        printf("%d\t", i);
     }
 
     return 0;
